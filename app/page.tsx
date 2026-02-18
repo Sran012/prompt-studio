0863d946181c3ae6582b31f291eacc6aef337c1d
Sran012 <88543485+Sran012@users.noreply.github.com>
2026-02-18 18:18:04 +0000

    using react markdown for rendering formatted optimised prompt

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 import { useRef, useState } from "react";
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
 
 export default function Home() {
   const [prompt, setPrompt] = useState("");
@@ -70,7 +72,7 @@ async function submit() {
 
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen w-screen bg-gray-900">
+    <div className="flex flex-col items-center justify-center min-h-screen w-screen bg-gray-900">
       <div className="">
          <input value={prompt} onChange={(e)=> {setPrompt(e.target.value)}} className="h-10 w-40 border-black bg-white text-black px-2"/>
          {loading && (
@@ -81,12 +83,18 @@ async function submit() {
           Cancel
         </button>
       )}
-         <button onClick={submit} className="h-10 w-20 bg-blue-500 text-white ml-2">{loading ? "optimize": "optimizing"}</button>
+         <button onClick={submit} className="h-10 w-20 bg-blue-500 text-white ml-2">{loading ? "optimizing": "optimize"}</button>
       </div>
        <div className="">
         {error && <p className="text-red-500 mt-4">{error}</p>}
         <h2 className="text-lg font-bold mt-4 text-white">Optimized Prompt:</h2>
-        <p className="mt-2 text-white">{optimizedPrompt}</p>
+        {/* <p className="mt-2 text-white">{optimizedPrompt}</p> */}
+        <div className="mx-auto bg-gray-800 p-4 rounded mt-2 text-white max-h-[600] overflow-auto">
+          <ReactMarkdown remarkPlugins={[remarkGfm]}>
+            {optimizedPrompt}
+          </ReactMarkdown>
+        </div>
+
        </div>
     </div>
   );
